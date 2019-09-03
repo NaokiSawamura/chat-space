380d26c6d8b4e5f7781585b92721addc9733a9e6
NaokiSawamura <n.sawamura48@gmail.com>
2019-09-03 14:51:21 +0900

    syuusei

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,11 +8,11 @@ set :repo_url, "git@example.com:me/my_repo.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '<このアプリで使用しているrubyのバージョン>' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' #!!!!!!!!カリキュラム通りに進めた場合、2.5.1か2.3.1です!!!!!!!!
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<ローカルPCのEC2インスタンスのSSH鍵(pem)へのパス>']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.shh/naoki48.pem']  #※例：~/.ssh/key_pem.pem
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
